3464cbc06297fd4e611ae178325a29451e2a0934
Efrosine <efrosinegames@gmail.com>
2024-02-16 22:13:44 +0700

    set up bloc for first time

---
+++ lib/features/main_feature/presentation/bloc/main_feature_event.dart
@@ -0,0 +1,8 @@
+part of 'main_feature_bloc.dart';
+
+abstract class MainFeatureEvent extends Equatable {
+  const MainFeatureEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/features/main_feature/presentation/bloc/main_feature_state.dart
@@ -0,0 +1,9 @@
+part of 'main_feature_bloc.dart';
+
+abstract class MainFeatureState extends Equatable {
+  const MainFeatureState();  
+
+  @override
+  List<Object> get props => [];
+}
+class MainFeatureInitial extends MainFeatureState {}
