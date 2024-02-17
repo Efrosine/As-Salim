6458874377141f600d18dc7eb02cf19dfe53a836
AbdillahBS <abdil.dewa@gmail.com>
2024-02-17 16:00:21 +0700

    fix on boarding

--- lib/features/main_feature/presentation/pages/onboarding_page.dart
+++ lib/features/main_feature/presentation/pages/onboarding_page.dart
@@ -1,4 +1,7 @@
+import 'package:assalim/config/constants/color.dart';
 import 'package:assalim/core/common/custom_background/base_background.dart';
+import 'package:assalim/core/common/custom_widget/backgroud_widget.dart';
+import 'package:assalim/features/main_feature/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingPage extends StatelessWidget {
@@ -6,48 +9,52 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          // Latar Belakang dari BaseBackground
-          BaseBackground.varian1(),
-
-          // Widget lain di atas latar belakang
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 80,
-                  child: Icon(
-                    Icons.person,
-                    size: 80,
-                    color: Colors.white,
-                  ),
+    return BackgourdWidget.linear(
+        linearGradient: [CColors.gradientVariant1, CColors.gradientVariant2],
+        child: Scaffold(
+            backgroundColor: Colors.transparent,
+            body: Container(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 80,
+                      child: Icon(
+                        Icons.person,
+                        size: 80,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Text("As-Saalim",
+                        style: Theme.of(context).textTheme.displayMedium),
+                    Text("Selamat Datang",
+                        style: Theme.of(context).textTheme.headlineSmall),
+                    SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Login_Page()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF00687A),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        minimumSize: Size(296, 40),
+                      ),
+                      child: Text("Get Started",
+                          style: Theme.of(context)
+                              .textTheme
+                              .labelLarge!
+                              .copyWith(color: Colors.white)),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 40),
-                Text("As-Saalim",
-                    style: TextStyle(fontSize: 35, color: Colors.black)),
-                Text("Selamat Datang",
-                    style: TextStyle(fontSize: 18, color: Colors.black)),
-                SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFF00687A),
-                  ),
-                  width: 296,
-                  height: 40,
-                  child: Center(
-                    child: Text("Get Started",
-                        style: TextStyle(color: Colors.white)),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+              ),
+            )));
   }
 }
