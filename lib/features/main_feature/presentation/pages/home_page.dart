a0e2a784184c2c9ab62318ef4c0f84274f8c6201
AbdillahBS <abdil.dewa@gmail.com>
2024-02-17 22:46:47 +0700

    homecoba

--- lib/features/main_feature/presentation/pages/home_page.dart
+++ lib/features/main_feature/presentation/pages/home_page.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Homfasdfasdfasdfsadfe'),
       ),
       body: const Center(
         child: Text('Home Page'),
