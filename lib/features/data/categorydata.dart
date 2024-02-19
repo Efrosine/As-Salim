606ef2f7d182d5700e7625caa014ef028db2d765
AbdillahBS <abdil.dewa@gmail.com>
2024-02-19 23:46:03 +0700

    belum fix

---
+++ lib/features/data/categorydata.dart
@@ -0,0 +1,44 @@
+import 'package:assalim/features/data/doctordata.dart';
+import 'package:assalim/features/main_feature/presentation/pages/chat_page.dart';
+import 'package:assalim/features/main_feature/presentation/pages/healtypage.dart';
+import 'package:assalim/features/main_feature/presentation/pages/monitor_blood_pressure_page.dart';
+import 'package:assalim/features/main_feature/presentation/pages/monitor_medicine_comsumption_page.dart';
+import 'package:assalim/features/main_feature/presentation/pages/monitor_water_comsumtion_page.dart';
+import 'package:assalim/features/main_feature/presentation/pages/nutritional_needs_page.dart';
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class CategoryData {
+  final String name;
+  final IconData icon;
+  final Widget page;
+
+  CategoryData({required this.name, required this.icon, required this.page});
+}
+
+List<CategoryData> categories = [
+  CategoryData(
+      name: "Konsultasi",
+      icon: Icons.support_agent,
+      page: ChatPage(
+        doctor: doctors[0],
+      )),
+  CategoryData(
+      name: "Cek Tekanan dan Gula Darah",
+      icon: MdiIcons.heartPulse,
+      page: MonitorBloodPressurePage()),
+  CategoryData(
+      name: "Kebutuhan Air Mineral",
+      icon: MdiIcons.beer,
+      page: MonitorWaterConsumtionPage()),
+  CategoryData(
+      name: "Kebutuhan Nutrisi",
+      icon: MdiIcons.foodApple,
+      page: NutritionalNeedsPage()),
+  CategoryData(
+      name: "Cek Kepatuhan Obat",
+      icon: MdiIcons.clipboardTextClockOutline,
+      page: MonitorMedicineConsumptionPage()),
+  CategoryData(
+      name: "Program Kebugaran", icon: MdiIcons.runFast, page: HealthyPage()),
+];
---
+++ lib/features/data/doctordata.dart
@@ -0,0 +1,34 @@
+class DoctorData {
+  final String name;
+  final String specialization;
+  final String image;
+
+  DoctorData({
+    required this.name,
+    required this.specialization,
+    required this.image,
+  });
+}
+
+List<DoctorData> doctors = [
+  DoctorData(
+    name: "Dr. Fajar Triatmojo, SpOG",
+    specialization: "Consultant - Spesialis Kandungan",
+    image: "assets/images/doctor1.jpg",
+  ),
+  DoctorData(
+    name: "Dr. Abdillah, Sp.A",
+    specialization: "Consultant - Spesialis Anak",
+    image: "assets/images/doctor2.jpg",
+  ),
+  DoctorData(
+    name: "Dr. Syifa, SpTHT",
+    specialization: "Consultant - Spesialis THT",
+    image: "assets/images/doctor3.jpg",
+  ),
+  DoctorData(
+    name: "Dr. Bintang Surya, SpKJ",
+    specialization: "Consultant - Spesialis Kedokteran Jiwa",
+    image: "assets/images/doctor4.jpg",
+  ),
+];
