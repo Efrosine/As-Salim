c6891ab7769d1dd947ff4f3e1920ef0cf4f3a39a
AbdillahBS <abdil.dewa@gmail.com>
2024-02-23 08:34:41 +0700

    create navbar

--- lib/features/main_feature/presentation/pages/navbar_page.dart
+++ lib/features/main_feature/presentation/pages/navbar_page.dart
@@ -1,17 +1,44 @@
+import 'package:assalim/features/main_feature/presentation/pages/main_pages.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class NavBarPage extends StatelessWidget {
+class NavBarPage extends StatefulWidget {
   const NavBarPage({super.key});
 
   @override
+  State<NavBarPage> createState() => _NavBarPageState();
+}
+
+class _NavBarPageState extends State<NavBarPage> {
+  int _curentIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('NavBar'),
+      bottomNavigationBar: NavigationBar(
+        destinations: listLabel,
+        selectedIndex: _curentIndex,
+        onDestinationSelected: (value) {
+          setState(() {
+            _curentIndex = value;
+          });
+        },
       ),
-      body: const Center(
-        child: Text('NavBar Page'),
-      ),
+      body: [
+        HomePage(),
+        ListDoctorPage(),
+        NewsPage(),
+        ProfilePage(),
+      ][_curentIndex],
     );
   }
+
+  List<NavigationDestination> listLabel = [
+    NavigationDestination(
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    NavigationDestination(icon: Icon(MdiIcons.doctor), label: "Doctors"),
+    NavigationDestination(icon: Icon(Icons.newspaper), label: 'News'),
+    NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+  ];
 }
